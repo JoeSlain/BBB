e419506a70f6ab3f4afc2e2a03838584fb13601e
Sylvain Taverniers <55266056+callme-sy@users.noreply.github.com>
2021-02-05 09:48:28 +0000

    USDT -> BTC ?

--- order_maker.py
+++ order_maker.py
@@ -5,7 +5,7 @@ def place_order(coin, amount):
     #print(symbz)
     client = Client("YOUR BINANCE API KEY", "YOUR BINANCE SECRET KEY")
     order = client.create_test_order(
-    symbol=coin.upper()+'USDT',
+    symbol=coin.upper()+'BTC',
     side=Client.SIDE_BUY,
     type=Client.ORDER_TYPE_MARKET,
     quantity=amount)
